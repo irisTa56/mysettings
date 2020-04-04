7bb65af1b904c18bc3a132c9cd994188ed39d831
Takayuki Kobayashi <iris.takayuki@gmail.com>
2020-04-04 12:11:54 +0900

    add a function to update all pip packages

--- shellrc_osx.sh
+++ shellrc_osx.sh
@@ -1,5 +1,12 @@
 alias ls='ls --color=auto --show-control-char'
 
+function pip-u-all() {
+  pip list --outdated --format=freeze \
+  | grep -v '^\-e' \
+  | cut -d = -f 1 \
+  | xargs -n1 pip install -U
+}
+
 function npm-g-i() {
   ASDF_SKIP_RESHIM=1 npm -g i $@
   asdf reshim nodejs
