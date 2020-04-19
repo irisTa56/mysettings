ddb7b28444cfac80077ec66f5ae7a84d5f5f1eef
Takayuki Kobayashi <iris.takayuki@gmail.com>
2020-04-19 16:16:27 +0900

    update jupyter image

--- docker/compose/dc.sh
+++ docker/compose/dc.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
 # “dc” stands for Docker-Compose
 
-if [ "$1" = "hadoop" ]; then
+if [ "$1" = "single" ]; then
+  docker-compose -f jupyter/docker-compose.yml ${@:2}
+elif [ "$1" = "hadoop" ]; then
   docker-compose -f hadoop/docker-compose.yml -f jupyter/docker-compose.yml ${@:2}
 fi
