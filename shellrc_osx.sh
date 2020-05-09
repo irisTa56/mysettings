c3e272a3e5b05e6a7224e3faf5ca3f5d611e622b
Takayuki Kobayashi <iris.takayuki@gmail.com>
2020-05-09 17:15:29 +0900

    add a missing package for antikythera-elixir image

--- shellrc_osx.sh
+++ shellrc_osx.sh
@@ -16,3 +16,13 @@ function npm-g-up() {
   ASDF_SKIP_RESHIM=1 npm -g up $@
   asdf reshim nodejs
 }
+
+function docker-run-with-gear() {
+  gear_dir=$1
+  ssh_auth_sock="/run/host-services/ssh-auth.sock"
+  docker run -it --rm \
+    --mount "type=bind,src=$gear_dir,dst=/root/$(basename $gear_dir)" \
+    -v "$ssh_auth_sock:$ssh_auth_sock" \
+    -e SSH_AUTH_SOCK="$ssh_auth_sock" \
+    irista56/antikythera-elixir
+}
