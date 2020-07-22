c393c69715f7be5767bdb0b537bede758510f7b1
irisTa56 <takayuki.kobayashi@access-company.com>
2020-07-22 17:14:30 +0900

    2020/07/22

--- update_installed_packages.sh
+++ update_installed_packages.sh
@@ -2,7 +2,7 @@
 set -exo pipefail
 
 brew leaves | tee installed_packages/homebrew.txt
-brew cask list | sed -e 's/\s+/\n/g' | tee installed_packages/homebrew_cask.txt
+# brew cask list | sed -e 's/\s+/\n/g' | tee installed_packages/homebrew_cask.txt
 cat ~/.tool-versions | tee installed_packages/asdf.txt
 pip freeze | tee installed_packages/pip.txt
 npm -g list --depth=0 | grep -oP '(?<=\s).*' | tee installed_packages/npm.txt
